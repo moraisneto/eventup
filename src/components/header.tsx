4dcbaaf3e0baccf8ebba6503d45a03699cfe04d1
José Morais <moraisneto@users.noreply.github.com>
2024-04-09 04:14:34 +0000

    Implementando o desing da página inicial

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,15 @@ import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
 
 export function Header() {
   return (
-    <div>
+    <div className="flex items-center gap-5 py-2">
       <img src={nlwUniteIcon} />
-      <nav>
-        <a href="">Eventos</a>
-        <a href="">Participantes</a>
+      <nav className="flex items-center gap-5">
+        <a href="" className="font-medium text-sm text-zinc-300">
+          Eventos
+        </a>
+        <a href="" className="font-medium text-sm">
+          Participantes
+        </a>
       </nav>
     </div>
   );
